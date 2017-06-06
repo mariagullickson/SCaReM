98784d84c37941c11a3520a9a9fbf442c5f7430e
Maria Gullickson <mariagullickson@gmail.com>
2017-06-06 15:55:30 -0400

    better multi-select for resources

--- SCaReM/static/js/reservation.js
+++ SCaReM/static/js/reservation.js
@@ -12,19 +12,20 @@ function allDay() {
 }
 
 // resource multiselect
-$('#id_resources').chosen();
+$('#id_resources').multiSelect();
 
 // filtering resources by tag
 $('#id_tag').change(function() {
     if ($('#id_tag').val()) {
-	$("#id_resources option").hide();
+	$("#ms-id_resources .ms-elem-selectable").hide();
 	var resources = tagResources[$('#id_tag').val()];
 	for (i = 0; i < resources.length; i++)
 	{
 	    var resource_id = resources[i];
-	    $("#id_resources option[value=" + resource_id + "]").show();
+	    $(".ms-selectable #" + resource_id + "-selectable").show();
+	    $(".ms-selectable #" + resource_id + "-selectable.ms-selected").hide();
 	}
     } else {
-	$("#id_resources option").show();
+	$("#ms-id_resources .ms-elem-selectable").show();
     }
 });
